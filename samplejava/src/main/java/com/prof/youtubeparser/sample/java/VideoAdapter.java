b6d707e853364ed3c7d0721e0f966713c82c752f
Marco Gomiero <marco.gomiero.93@gmail.com>
2019-05-20 21:53:15 +0200

    Restore and improve java example

---
+++ samplejava/src/main/java/com/prof/youtubeparser/sample/java/VideoAdapter.java
@@ -0,0 +1,165 @@
+/*
+ *   Copyright 2016 Marco Gomiero
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package com.prof.youtubeparser.sample.java;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.prof.youtubeparser.VideoStats;
+import com.prof.youtubeparser.models.stats.Statistics;
+import com.prof.youtubeparser.models.videos.Video;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> {
+
+    private List<Video> videos;
+    private MainActivity mActivity;
+
+    public VideoAdapter(List<Video> list, MainActivity activity) {
+        this.videos = list;
+        this.mActivity = activity;
+    }
+
+    public void clearData() {
+        if (videos != null)
+            videos.clear();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_video, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        final Video currentVideo = videos.get(position);
+
+        String pubDateString = currentVideo.getDate();
+        final String videoTitle = currentVideo.getTitle();
+
+        //retrieve video link
+        final String videoId = currentVideo.getVideoId();
+        final String link = "https://www.youtube.com/watch?v=" + videoId;
+
+        viewHolder.title.setText(currentVideo.getTitle());
+        viewHolder.pubDate.setText(pubDateString);
+
+        Picasso.get()
+                .load(currentVideo.getCoverLink())
+                .placeholder(R.drawable.placeholder)
+                .fit()
+                .centerCrop()
+                .into(viewHolder.image);
+
+        //show statistic of the selected video
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mActivity.getVideoStats(videoId);
+                // TODO: move to viewmodel
+
+               /* VideoStats videoStats = new VideoStats();
+                videoStats.onFinish(new VideoStats.OnTaskCompleted() {
+                    @Override
+                    public void onTaskCompleted(@NonNull Statistics stats) {
+                        String body = "Views: " + stats.getViewCount() + "\n" +
+                                "Like: " + stats.getLikeCount() + "\n" +
+                                "Dislike: " + stats.getDislikeCount() + "\n" +
+                                "Number of comment: " + stats.getCommentCount() + "\n" +
+                                "Number of favourite: " + stats.getFavoriteCount();
+
+                        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(mContext);
+                        dialogBuilder.setTitle("Stats for: \"" + videoTitle + "\"");
+                        dialogBuilder.setMessage(body);
+                        dialogBuilder.setCancelable(false);
+                        dialogBuilder.setNegativeButton(android.R.string.ok,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        dialogBuilder.show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(mContext, "Unable to get statistic for this video. Please try again", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                String requestUrl = videoStats.generateStatsRequest(videoId, BuildConfig.KEY);
+                videoStats.execute(requestUrl);*/
+            }
+        });
+
+        //open the video on Youtube
+        viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                mActivity.startActivity(intent1);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return videos == null ? 0 : videos.size();
+    }
+
+    public List<Video> getList() {
+        return videos;
+    }
+
+    public void setVideos(List<Video> videos) {
+        this.videos = videos;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        TextView pubDate;
+        ImageView image;
+
+        ViewHolder(View itemView) {
+
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            pubDate = itemView.findViewById(R.id.pubDate);
+            image = itemView.findViewById(R.id.image);
+        }
+    }
+}
